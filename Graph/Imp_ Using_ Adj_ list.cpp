b22edd49ad26fc2601d5ddd0c578d37c744498d4
Amar Narayan Dwivedi <119877633+AmarNarayanDwivedi@users.noreply.github.com>
2024-09-07 23:55:29 +0530

    Adding comments

--- Graph/Imp_ Using_ Adj_ list.cpp
+++ Graph/Imp_ Using_ Adj_ list.cpp
@@ -1,54 +1,72 @@
-// Online C++ compiler to run C++ program online
 #include <iostream>
 #include <list>
 #include <unordered_map>
+
 using namespace std;
 
+// Define a class to represent the graph
 class graph
 {
 public:
+    // Adjacency list: maps a node (key) to a list of connected nodes (values)
     unordered_map<int, list<int>> adj;
 
+    // Method to add an edge to the graph
+    // 'u' and 'v' are the nodes (vertices) to be connected
+    // 'direction' indicates whether the graph is directed (1) or undirected (0)
     void addEdge(int u, int v, bool direction)
     {
+        // Add edge from u to v
         adj[u].push_back(v);
+        // If the graph is undirected (direction == 0), also add edge from v to u
         if (direction == 0)
         {
             adj[v].push_back(u);
         }
     }
 
+    // Method to print the adjacency list
     void printlist()
     {
+        // Iterate through each node and its list of connected nodes
         for (auto i : adj)
         {
+            // Print the current node
             cout << i.first << " --> ";
+            // Print all nodes connected to the current node
             for (auto j : i.second)
             {
                 cout << " " << j;
             }
+            // Move to the next line after printing all connected nodes for the current node
             cout << endl;
         }
     }
 };
+
 int main()
 {
     int n;
-    cout << "Enter the number of nodes " << endl;
-    cin >> n;
-    int m;
-    cout << "Enter the number of edges" << endl;
-    cin >> m;
+    cout << "Enter the number of nodes: " << endl;
+    cin >> n; // Read the number of nodes
 
+    int m;
+    cout << "Enter the number of edges: " << endl;
+    cin >> m; // Read the number of edges
+
+    // Create an instance of the graph class
     graph g;
 
+    // Read edges and add them to the graph
     for (int i = 0; i < m; i++)
     {
         int u, v;
-        cin >> u >> v;
-        g.addEdge(u, v, 0);
+        cin >> u >> v;      // Read each edge (u, v)
+        g.addEdge(u, v, 0); // Add the edge to the graph (undirected graph)
     }
 
+    // Print the adjacency list of the graph
     g.printlist();
+
     return 0;
 }
